7bf71e036c7a8d403c4e7d535ae2a95cbf122d4b
htatuan <tuan145514@gmail.com>
2022-05-16 15:24:07 +0900

    hi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DangNhapComponent } from './dang-nhap/dang-nhap.component';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'TrangBanHang', component: TrangChuComponent},
+  {path: 'DangNhap', component: DangNhapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
