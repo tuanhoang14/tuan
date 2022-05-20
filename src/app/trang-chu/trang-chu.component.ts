b686301bbc674a5962e82acdd611f650c06a11a1
htatuan <tuan145514@gmail.com>
2022-05-20 10:06:04 +0900

    Css

--- src/app/trang-chu/trang-chu.component.ts
+++ src/app/trang-chu/trang-chu.component.ts
@@ -17,7 +17,6 @@ export class TrangChuComponent implements OnInit {
   soTrang = 0;
   sortName : Sort = {active:'name',direction:'asc'} ;
   sortPrice : Sort = {active:'price',direction:'asc'} ;
-  color = "";
   dataSource1 = filterTest(this.filter);
   itemPage = paginationPage(this.dataSource1, this.soTrang);
   page = 0;
@@ -37,10 +36,9 @@ export class TrangChuComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    filterTest(this.filter);
   }
 
-  filterTest1(value: any){
+  filterTest(value: any){
     this.dataSource1 = ELEMENT_DATA.filter(data => data.name.includes(value));
     this.soTrang = 0;
     this.itemPage = paginationPage(this.dataSource1, this.soTrang);
@@ -48,9 +46,6 @@ export class TrangChuComponent implements OnInit {
 
   sortData(sort: Sort) {
     const data = this.dataSource1.slice();
-    console.log( this.soTrang);
-    console.log( this.itemPage );
-    console.log( this.dataSource1);
     if (!sort.active || sort.direction === '') {
       this.dataSource1 = data;
       return;
@@ -73,11 +68,6 @@ export class TrangChuComponent implements OnInit {
     });
     this.soTrang = 0;
     this.itemPage = paginationPage(this.dataSource1, this.soTrang);
-
-    console.log( this.soTrang);
-    console.log( this.itemPage );
-    console.log( this.dataSource1);
-
   }
 
   TrangSau() {
@@ -121,24 +111,24 @@ export interface PeriodicElement {
   color: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 1, name: 'Hydrogen', price: 1.0079, mieuTa: 'H',color:"null"},
-  {id: 2, name: 'Helium', price: 4.0026, mieuTa: 'He',color:"null"},
-  {id: 3, name: 'Lithium', price: 6.941, mieuTa: 'Li',color:"null"},
-  {id: 4, name: 'Beryllium', price: 9.0122, mieuTa: 'Be',color:"null"},
-  {id: 5, name: 'Boron', price: 12.811, mieuTa: 'B',color:"null"},
-  {id: 6, name: 'Carbon', price: 12.0127, mieuTa: 'C',color:"null"},
-  {id: 7, name: 'Nitrogen', price: 14.0067, mieuTa: 'N',color:"null"},
-  {id: 8, name: 'Oxygen', price: 15.9994, mieuTa: 'O',color:"null"},
-  {id: 9, name: 'Fluorine', price: 18.9984, mieuTa: 'F',color:"null"},
-  {id: 12, name: 'Neon', price: 20.1797, mieuTa: 'Ne',color:"null"},
-  {id: 11, name: 'Sodium', price: 22.9897, mieuTa: 'Na',color:"null"},
-  {id: 12, name: 'Magnesium', price: 24.305, mieuTa: 'Mg',color:"null"},
-  {id: 13, name: 'Aluminum', price: 26.9815, mieuTa: 'Al',color:"null"},
-  {id: 14, name: 'Silicon', price: 28.0855, mieuTa: 'Si',color:"null"},
-  {id: 15, name: 'Phosphorus', price: 30.9738, mieuTa: 'P',color:"null"},
-  {id: 16, name: 'Sulfur', price: 32.065, mieuTa: 'S',color:"null"},
-  {id: 17, name: 'Chlorine', price: 35.453, mieuTa: 'Cl',color:"null"},
-  {id: 18, name: 'Argon', price: 39.948, mieuTa: 'Ar',color:"null"},
-  {id: 19, name: 'Potassium', price: 39.0983, mieuTa: 'K',color:"null"},
-  {id: 20, name: 'Calcium', price: 40.078, mieuTa: 'Ca',color:"null"},
+  {id: 1, name: 'Hydrogen', price: 1.0079, mieuTa: 'ABC',color:"null"},
+  {id: 2, name: 'Helium', price: 4.0026, mieuTa: 'ABC',color:"null"},
+  {id: 3, name: 'Lithium', price: 6.941, mieuTa: 'ABC',color:"null"},
+  {id: 4, name: 'Beryllium', price: 9.0122, mieuTa: 'BABCe',color:"null"},
+  {id: 5, name: 'Boron', price: 12.811, mieuTa: 'BABC',color:"null"},
+  {id: 6, name: 'Carbon', price: 12.0127, mieuTa: 'ABCC',color:"null"},
+  {id: 7, name: 'Nitrogen', price: 14.0067, mieuTa: 'ABCN',color:"null"},
+  {id: 8, name: 'Oxygen', price: 15.9994, mieuTa: 'OABC',color:"null"},
+  {id: 9, name: 'Fluorine', price: 18.9984, mieuTa: 'ABCF',color:"null"},
+  {id: 12, name: 'Neon', price: 20.1797, mieuTa: 'NABCe',color:"null"},
+  {id: 11, name: 'Sodium', price: 22.9897, mieuTa: 'NaABC',color:"null"},
+  {id: 12, name: 'Magnesium', price: 24.305, mieuTa: 'MABCg',color:"null"},
+  {id: 13, name: 'Aluminum', price: 26.9815, mieuTa: 'ABCAl',color:"null"},
+  {id: 14, name: 'Silicon', price: 28.0855, mieuTa: 'SABCi',color:"null"},
+  {id: 15, name: 'Phosphorus', price: 30.9738, mieuTa: 'ABCP',color:"null"},
+  {id: 16, name: 'Sulfur', price: 32.065, mieuTa: 'SABC',color:"null"},
+  {id: 17, name: 'Chlorine', price: 35.453, mieuTa: 'CABCl',color:"null"},
+  {id: 18, name: 'Argon', price: 39.948, mieuTa: 'AABC',color:"null"},
+  {id: 19, name: 'Potassium', price: 39.0983, mieuTa: 'ABCK',color:"null"},
+  {id: 20, name: 'Calcium', price: 40.078, mieuTa: 'CABC',color:"null"},
 ];
