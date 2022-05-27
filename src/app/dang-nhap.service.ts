d9edc56e9741e91641af5750bb6d2758bf82ea26
htatuan <tuan145514@gmail.com>
2022-05-27 09:58:05 +0900

    fix login

--- src/app/dang-nhap.service.ts
+++ src/app/dang-nhap.service.ts
@@ -18,3 +18,27 @@ export class DangNhapService {
     return false;
   }
 }
+export interface TaiKhoan {
+  name: string;
+  id: number;
+  matKhau: string;
+}
+
+const ELEMENT_DATA: TaiKhoan[] = [
+  {id: 1, name: 'Hydrogen',matKhau: "null"},
+  {id: 2, name: 'Helium', matKhau:"null"},
+  {id: 3, name: 'Lithium', matKhau:"null"},
+  {id: 4, name: 'Beryllium', matKhau:"null"},
+  {id: 5, name: 'Boron', matKhau:"null"},
+  {id: 6, name: 'Carbon', matKhau:"null"},
+  {id: 7, name: 'Nitrogen', matKhau:"null"},
+  {id: 8, name: 'Oxygen', matKhau:"null"},
+  {id: 9, name: 'Fluorine', matKhau:"null"},
+  {id: 12, name: 'Neon', matKhau:"null"},
+  {id: 11, name: 'Sodium', matKhau:"null"},
+  {id: 12, name: 'Magnesium', matKhau:"null"},
+  {id: 13, name: 'Aluminum', matKhau:"null"},
+  {id: 14, name: 'Silicon', matKhau:"null"},
+  {id: 15, name: 'Phosphorus', matKhau:"null"},
+  {id: 16, name: 'Sulfur', matKhau:"null"},
+];
